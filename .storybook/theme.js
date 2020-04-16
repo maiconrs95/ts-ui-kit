bc3cf2037f86bc19a23fb7665784d1fdf5a8abb1
Maicon Silva <maiconrs95@gmail.com>
2020-04-16 18:27:56 -0300

    initial commit

---
+++ .storybook/theme.js
@@ -0,0 +1,22 @@
+import { create } from '@storybook/theming';
+
+export default create({
+    base: 'dark',
+
+    colorSecondary: '#00A5DB',
+
+    appBg: '#2D2D2D',
+    appContentBg: '#575757',
+    appBorderColor: '#00A5DB',
+
+    textColor: '#fff',
+
+
+    // Toolbar and active colors
+    barTextColor: '#e6e6e6',
+    barSelectedColor: '#fff',
+    barBg: '#1b1b1f',
+
+    brandTitle: 'TS UI KIT',
+    brandUrl: 'https://www.ingresse.com',
+});
