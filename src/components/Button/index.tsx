ad5553b05e5925ae7fd1993efaada676a247a40e
Maicon Silva <maiconrs95@gmail.com>
2020-04-16 20:15:36 -0300

    finish base config

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+interface Props {
+    /**
+     * Background color
+     */
+    backgroundColor?: string;
+
+    /**
+     * Color
+     */
+    color: string;
+}
+
 const style: React.CSSProperties = {
     backgroundColor: '#00A5DB',
     color: '#fff',
 };
 
-export const Button: React.FC = ({ children }) => {
+export const Button: React.FC<Props> = ({
+    children,
+    backgroundColor = '#00A5DB',
+    color
+}) => {
     return <button style={style}>{children}</button>;
 };
